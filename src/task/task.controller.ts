0e0b9c10bc5030da38eafa0f35118170a87bb2f7
Maysa Araujo <maysaaraujo25@gmail.com>
2025-08-02 12:51:24 -0300

    feat: create task find by id and update

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { TaskDto } from './task.dto';
+import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
   @Post()
   create(@Body() task: TaskDto) {
-    console.log(task);
+    this.taskService.create(task);
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string): TaskDto {
+    return this.taskService.findById(id);
+  }
+
+  @Put()
+  update(@Body() task: TaskDto) {
+    this.taskService.update(task);
   }
 }
